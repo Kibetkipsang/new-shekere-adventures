a4f6adf802968a7963c075e5da90a7df8b0da737
Kibet Kipsang <denniskipsang2000@gmail.com>
2025-05-17 15:22:21 +0300

    added tour packages model

--- server/app/models.py
+++ server/app/models.py
@@ -22,3 +22,17 @@ class Destination(db.Model):
     image_url = db.Column(db.String(250))
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
+
+class TourPackage(db.Model):
+    __tablename__ = 'tourpackages'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    details = db.Column(db.Text, nullable=False)
+    duration = db.Column(db.String(50))
+    image_url = db.Column(db.String(250))
+
+    destintion_id = db.Column(db.Integer, db.ForeignKey('destination_id'), nullable = False)
+    guide_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    destination = db.relationship('Destination', backref='packages')
+    guide = db.relationship('User', backref='guided_packages')
